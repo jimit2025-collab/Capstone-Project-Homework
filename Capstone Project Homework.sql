ffa3b127a346341925db76314840518ca4673466
jimit2025-collab <shahjded@gmail.com>
2025-08-07 10:42:48 -0400

    Add files via upload

---
+++ Capstone Project Homework.sql
@@ -0,0 +1,13 @@
+CREATE TABLE EMPLOYEES (
+    EMPLOYEES_ID INT,
+    EMPLOYEES_NAME TEXT,
+    DEPARTMENT_ID TEXT,
+    SALARY_ID INT
+);
+
+INSERT INTO EMPLOYEES (EMPLOYEES_ID, EMPLOYEES_NAME, DEPARTMENT_ID, SALARY_ID) VALUES 
+    (1, 'Noah', 'Sales', 40000),
+    (2, 'Bob', 'IT', 35000),
+    (3, 'Harsh', 'Sales', 50000),
+    (4, 'Rita', 'IT', 45000),
+    (5, 'William', 'Sales', 30000);
